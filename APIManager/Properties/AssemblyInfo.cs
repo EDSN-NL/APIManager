e6fb16e17b2e7e07560a6812b702d61b8f3d85c6
Wouter Meijers <wouter.meijers@nebulon.nl>
2017-12-12 18:10:20 +0100

    Removed absolute path to WiXInstaller. Created MSI.

--- APIManager/Properties/AssemblyInfo.cs
+++ APIManager/Properties/AssemblyInfo.cs
@@ -1,26 +1,24 @@
-﻿using System.Reflection;
+using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("API Manager")]
-[assembly: AssemblyDescription("API Manager is a Sparx Enterprise Architect plugin that is used to create and/or otherwise manage formal API specifications from UML models.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Enexis ICT - BaVo Vernieuwing")]
-[assembly: AssemblyProduct("APIManager")]
-[assembly: AssemblyCopyright("Copyright ©  2017, Enexis ICT")]
-[assembly: AssemblyTrademark("Created for Enexis by Nebulon b.v. (info@nebulon.nl)")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("API Manager")]
+[assembly: AssemblyDescription ("API Manager is a Sparx Enterprise Architect plugin that is used to create and/or otherwise manage formal API specifications from UML models.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Enexis ICT - BaVo Vernieuwing")]
+[assembly: AssemblyProduct ("APIManager")]
+[assembly: AssemblyCopyright ("Copyright ©  2017, Enexis ICT")]
+[assembly: AssemblyTrademark ("Created for Enexis by Nebulon b.v. (info@nebulon.nl)")]
+[assembly: AssemblyCulture ("")]
 // Most of this solution is 'internal reference only'. The only components that MUST be 'COM-Visible' are the 
 // EAAddinFramework and EAController. So these are marked explicitly as ComVisible(true).
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("386c6da7-1c6c-45a1-a423-59dbccd4f0b4")]
-
+[assembly: Guid ("386c6da7-1c6c-45a1-a423-59dbccd4f0b4")]
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -31,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion ("1.1.0.1")]
+[assembly: AssemblyFileVersion ("1.1.0.1")]
