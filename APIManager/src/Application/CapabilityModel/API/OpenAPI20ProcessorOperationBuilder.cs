30a0cceefd5d024284282927c8b0cfd1cbb0f551
Wouter Meijers <wouter.meijers@nebulon.nl>
2018-02-08 12:32:27 +0100

    Rolename of intermediate association now ends in List in case of cardinality >1.

--- APIManager/src/Application/CapabilityModel/API/OpenAPI20ProcessorOperationBuilder.cs
+++ APIManager/src/Application/CapabilityModel/API/OpenAPI20ProcessorOperationBuilder.cs
@@ -653,8 +653,9 @@ namespace Plugin.Application.CapabilityModel.API
                 if (cardinality.Item2 == 0) assocCard += "..*";
                 else if (cardinality.Item2 != cardinality.Item1) assocCard += ".." + cardinality.Item2.ToString();
                 string roleName = !string.IsNullOrEmpty(payload.AliasName) ? payload.AliasName : payload.Name;
+                if ((cardinality.Item2 == 0 || cardinality.Item2 > 1) && !roleName.EndsWith("List")) roleName += "List";
                 var source = new EndpointDescriptor(paginationClass, "1", paginationClass.Name, null, false);
-                var target = new EndpointDescriptor(payload, assocCard, RESTUtil.GetAssignedRoleName(roleName), null, true);
+                var target = new EndpointDescriptor(payload, assocCard, roleName, null, true);
                 paginationClass.CreateAssociation(source, target, MEAssociation.AssociationType.MessageAssociation);
                 qualifiedClassName = this._schema.ProcessClass(paginationClass, paginationClass.Name);
                 cardinality = new Tuple<int, int>(1, 1);    // Response is mandatory object and actual cardinality with payload has been solved otherwise.
